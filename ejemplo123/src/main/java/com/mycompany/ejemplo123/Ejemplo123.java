790abac031ebee362485a0c467d14cde54efdc9f
aadriann12 <DAW128@IF03-29.iesmhp.local>
2024-12-02 14:54:40 +0100

    holaaa

---
+++ ejemplo123/src/main/java/com/mycompany/ejemplo123/Ejemplo123.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejemplo123;
+
+/**
+ *
+ * @author DAW128
+ */
+public class Ejemplo123 {
+
+    public static void main(String[] args) {
+        System.out.println("Holaa!");
+    }
+}
